15fa2ddbf490eddd6ad07b1d3a4cac549280cba8
ISSOtm <eldredhabert0@gmail.com>
2019-10-31 01:04:44 +0100

    Add `make dist`

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
+
+all: page.css
+.PHONY: all
+
 page.css: gen_page_css.py
 	./$< > $@
+
+
+dist: select-thy-language.zip
+.PHONY: dist
+
+include dist.mk
+select-thy-language.zip:
+	zip -FS $@ $^
---
+++ dist.mk
@@ -0,0 +1,14 @@
+select-thy-language.zip: button/cpp-19.png
+select-thy-language.zip: button/cpp-38.png
+
+select-thy-language.zip: icon/cpp-48.png
+select-thy-language.zip: icon/cpp-96.png
+
+select-thy-language.zip: popup/popup.css
+select-thy-language.zip: popup/popup.html
+select-thy-language.zip: popup/popup.js
+
+select-thy-language.zip: manifest.json
+select-thy-language.zip: page.css
+select-thy-language.zip: page.js
+select-thy-language.zip: stl.js
